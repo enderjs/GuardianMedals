2521ae8615978b631d76bb4d40de6293326c5241
ender <enderjs@gmail.com>
2017-10-19 11:37:31 -0400

    —Set for material, covalent, ngxcharts, flex layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
